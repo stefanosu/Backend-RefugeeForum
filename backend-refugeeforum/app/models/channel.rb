3879ec2bf54607ddff4bc823e9086aa73e1e75bb
Stefanos <stefanosu422@gmail.com>
2019-12-16 18:51:23 -0500

    added realtionships to models

--- backend-refugeeforum/app/models/channel.rb
+++ backend-refugeeforum/app/models/channel.rb
@@ -1,2 +1,4 @@
 class Channel < ApplicationRecord
+  belongs_to :user 
+  has_many :posts
 end
--- backend-refugeeforum/app/models/user.rb
+++ backend-refugeeforum/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  
+  has_many :channels
+  has_many :posts
+  has_many :comments
+
+  has_secure_password
 end
