cd843f23d26d9e6f90a3bf9c149a81f64e131637
Stefanos <stefanosu422@gmail.com>
2020-01-24 19:53:14 -0500

    cleaned up channels controller

--- backend-refugeeforum/app/controllers/api/v1/channels_controller.rb
+++ backend-refugeeforum/app/controllers/api/v1/channels_controller.rb
@@ -40,15 +40,12 @@ before_action :set_channel, only: [:show, :update, :destroy]
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
   def set_channel
     @channel = channel.find(params[:id])
   end
 
-    # Only allow a trusted parameter "white list" through.
   def channel_params
-      # byebug
-      params.permit(:title, :user_id)
+    params.permit(:title, :user_id)
   end
 
 end
