804f339a77a40c3b80412c79822386d2d12d5f3d
Stefanos <stefanosu422@gmail.com>
2020-01-29 16:49:38 -0500

    cleaned up channels controller

--- backend-refugeeforum/app/controllers/api/v1/channels_controller.rb
+++ backend-refugeeforum/app/controllers/api/v1/channels_controller.rb
@@ -25,7 +25,6 @@ before_action :set_channel, only: [:show, :update, :destroy]
     end
   end
 
-  # PATCH/PUT /channel/1
   def update
     if @channel.update(channel_params)
       render json: @channel
